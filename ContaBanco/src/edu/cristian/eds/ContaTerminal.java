e4bb89ac856905e1bd3aeaf4a527998011f8492a
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-05-11 21:48:09 -0300

    Realizado projeto, entradas do usuarios e frase de saida

--- ContaBanco/src/edu/cristian/eds/ContaTerminal.java
+++ ContaBanco/src/edu/cristian/eds/ContaTerminal.java
@@ -1,10 +1,31 @@
 package edu.cristian.eds;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Bem vindo ao Nosso Banco! Para registrar sua conta digite as informações solicitadas:");
+		System.out.print("Informe o número da conta: ");
+		int numeroConta = scanner.nextInt();
+		
+		System.out.print("Informe a agência: ");
+		String agencia = scanner.next();
+		
+		scanner.nextLine();
+		System.out.print("Informe seu nome completo: ");
+		String nomeCliente = scanner.nextLine();
+		
+		
+		System.out.print("Informe o saldo na conta: ");
+		double saldo = scanner.nextDouble();
+		
+		System.out.println("Olá "+ nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é "+agencia+" ,"
+				+ " conta "+numeroConta+" e seu saldo "+saldo+" já está disponível para saque!");
+		
 	}
-
 }
